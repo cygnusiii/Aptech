7ca7f88be7041b3ccc1e95d2c6468bf2692f6a89
cygnusiii <cygnus@3i.com.vn>
2014-07-19 21:24:55 +0700

    Update2 18-7-2014

---
+++ PCS/Lab7/Lab7_2/Lab7_2/DeviceBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab7_2
+{
+    abstract class DeviceBase
+    {
+        private int pro_id;
+        private string pro_name;
+
+        public int Pro_id
+        {
+            get { return pro_id; }
+        }
+
+
+
+        public string Pro_name
+        {
+            get { return pro_name; }
+            set
+            {
+                try
+                {
+                    pro_name = value;
+                    if (value.Length < 5) throw new Exception();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You must type pro_name with value length more than 5 characters");
+                }
+            }
+        }
+
+        public DeviceBase()
+        {
+        }
+        public DeviceBase(int pro_id)
+        {
+            this.pro_id = pro_id;
+            this.pro_name = "No name";
+        }
+
+        public DeviceBase(int pro_id, string pro_name)
+        {
+            this.pro_id = pro_id;
+            this.pro_name = pro_name;
+        }
+
+
+        abstract virtual void ShowInfo();
+
+    }
+}
---
+++ PCS/Lab7/Lab7_2/Lab7_2/IDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab7_2
+{
+    interface IDevice
+    {
+        string Manufacturer { set; get; }
+        int Price { set; get; }
+        float VAT { get; }
+        float NetPrice { get; }
+    }
+}
